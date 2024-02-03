1f02c90aa97e690c049604d2f1b431b80581f9ee
Michał <116152132+UrbanMikePL@users.noreply.github.com>
2024-02-03 11:37:40 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import { Footer, Header, Features, Blog, Possibility, WhatUrbanGPT } from './containers';
+import { Brand, CTA, Navbar } from './components';
+import './App.css';
+
+
+const App = () => {
+  return (
+
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+        
+      </div>
+       <WhatUrbanGPT />
+       <Possibility />
+       <Features />
+       <CTA />
+       <Blog />
+       <Footer/>
+       <Brand />
+    </div>
+  )
+}
+
+export default App
