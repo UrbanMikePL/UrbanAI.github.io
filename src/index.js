acb4a0f9c0ed2f5e436f937aa69650787df5b119
Michał <116152132+UrbanMikePL@users.noreply.github.com>
2024-02-05 11:20:21 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-import App from './App'
-import './index.css'
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
-
-
-
-
+reportWebVitals();
